1804da9238d14fbc8b6d9f73b6515590fc8a709d
Eric Schmar <10875517+ericschmar@users.noreply.github.com>
2017-12-03 21:54:02 -0600

    Added a total count of decks being tracked to be served

--- preprocess/coallate.py
+++ preprocess/coallate.py
@@ -6,6 +6,7 @@ from archetype import archetype
 import sqlite3
 from sqlite3 import Error
 
+total_decks = 0
 
 def parse_decks_json(file):
     with open(file) as data_file:
@@ -33,6 +34,8 @@ def parse_decks_json(file):
     return decks
 
 def count_archetypes(decks):
+    global total_decks
+    total_decks = len(decks)
     archs = {}
     for i in range(len(decks)):
         if decks[i].id not in archs:
@@ -43,6 +46,7 @@ def count_archetypes(decks):
     return archs
         
 def save_deck_counts(filename):
+    global total_decks
     conn = sqlite3.connect("hs-clock.db")
     c = conn.cursor()
     f = open(filename, 'w')
@@ -53,6 +57,7 @@ def save_deck_counts(filename):
         c.execute("SELECT id, name FROM archetype WHERE id=?", (sorted_decks[i][0], ))
         row = c.fetchall()
         top_archs[row[0][1]] = { "id":row[0][0], "count":sorted_decks[i][1] } 
+    top_archs["total"] = total_decks
 
     print top_archs
 
